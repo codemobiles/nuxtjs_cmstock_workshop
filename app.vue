d0ddd666e34d9a168a48e70081e9605ad1880aaa
alonespace <jomcootle@gmail.com>
2023-10-28 00:33:10 +0700

    ep4

--- app.vue
+++ app.vue
@@ -2,6 +2,10 @@
     <div>
         <h1 class="mydivclass">{{ msg }}</h1>
         <input type="text" placeholder="Search">
+        <h3>Image From public/</h3>
+        <img src="./images/dog.jpeg" alt="Dog logo">
+        <h3>Image From assets/</h3>
+        <img src="~/assets/images/mountain.jpg" alt="Dog logo">
     </div>
 </template>
 
