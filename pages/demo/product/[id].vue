8d0ed94857becd08617ec9aea36a448edbaae02c
alonespace <jomcootle@gmail.com>
2023-10-29 01:31:40 +0700

    ep7

---
+++ pages/demo/product/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Product Page</h1>
+        <h2>Product ID: {{ route.params.id }}</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<style scoped></style>
