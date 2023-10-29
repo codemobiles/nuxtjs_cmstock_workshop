ea17d26f5221a84cd7938c28a84d4c29ea006443
alonespace <jomcootle@gmail.com>
2023-10-29 19:31:39 +0700

    ep9

--- pages/demo/product/[id].vue
+++ pages/demo/product/[id].vue
@@ -4,11 +4,31 @@
         <h2>Product ID: {{ route.params.id }}</h2>
         <h3>Color: {{ route.query.color }}</h3>
         <h3>Brand: {{ route.query.brand }}</h3>
+        <NuxtLink active-class="active" to="/demo/product/1?color=black&brand=B"
+            >Home Demo</NuxtLink
+        >
+        <button @click="goTo('2')">Go to Product 2</button>
     </div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute();
+const router = useRouter();
+
+const goTo = (id: string) => {
+    router.push(`/demo/product/${id}?color=red&brand=A`);
+    // router.push({
+    //     path: `/demo/product/${id}`,
+    //     query: {
+    //         color: "red",
+    //         brand: "A",
+    //     },
+    // });
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.active {
+    color: red;
+}
+</style>
