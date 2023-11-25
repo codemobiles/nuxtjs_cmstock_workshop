89390395f0796af1379582df3b1a651b14d785cf
alonespace <jomcootle@gmail.com>
2023-11-26 00:07:11 +0700

    ep95

--- pages/stock/edit/[id].vue
+++ pages/stock/edit/[id].vue
@@ -1,7 +1,121 @@
 <template>
-    <div>
-        {{ route.params.id }}
-    </div>
+    <a-row class="tw-mb-4">
+        <a-col :span="24">
+            <a-card
+                class="tw-drop-shadow-md hover:tw-drop-shadow-lg tw-transition-all tw-rounded-lg"
+            >
+                <a-form
+                    :label-col="{ span: 24 }"
+                    :wrapper-col="{ span: 24 }"
+                    class="tw-w-full"
+                >
+                    <a-row align="center" justify="center">
+                        <a-col :span="16">
+                            <a-row
+                                align="center"
+                                justify="center"
+                                class="tw-w-full"
+                                :gutter="[10, 0]"
+                            >
+                                <a-col
+                                    :span="24"
+                                    class="tw-flex tw-justify-center"
+                                >
+                                    <a-tag
+                                        color="warning"
+                                        class="tw-font-medium tw-text-xl tw-rounded-lg tw-px-4 tw-mb-4"
+                                    >
+                                        EDIT PRODUCT
+                                    </a-tag>
+                                </a-col>
+                                <a-col
+                                    :span="24"
+                                    class="tw-flex tw-justify-center tw-w-full"
+                                >
+                                    <a-form-item
+                                        label="Name"
+                                        class="tw-font-medium tw-w-full"
+                                    >
+                                        <a-input />
+                                    </a-form-item>
+                                </a-col>
+                                <a-col
+                                    :span="12"
+                                    class="tw-flex tw-justify-center"
+                                >
+                                    <a-form-item
+                                        label="Price"
+                                        class="tw-font-medium tw-w-full"
+                                    >
+                                        <a-input />
+                                    </a-form-item>
+                                </a-col>
+                                <a-col
+                                    :span="12"
+                                    class="tw-flex tw-justify-center"
+                                >
+                                    <a-form-item
+                                        label="Stock"
+                                        class="tw-font-medium tw-w-full"
+                                    >
+                                        <a-input />
+                                    </a-form-item>
+                                </a-col>
+                                <a-col
+                                    :span="24"
+                                    class="tw-flex tw-justify-center"
+                                >
+                                    <a-form-item
+                                        label="Image file"
+                                        class="tw-font-medium tw-w-full"
+                                    >
+                                        <a-upload
+                                            :max-count="1"
+                                            name="image"
+                                            list-type="picture-card"
+                                            class="avatar-uploader"
+                                            :show-upload-list="true"
+                                        >
+                                            <img class="tw-p-1" alt="Upload" />
+                                            <!-- <div v-else>
+                        <loading-outlined
+                          v-if="stockStore.isLoading()"
+                        ></loading-outlined>
+                        <plus-outlined v-else></plus-outlined>
+                        <div class="ant-upload-text">Upload</div>
+                      </div> -->
+                                        </a-upload>
+                                        <a-modal :footer="null">
+                                            <img
+                                                alt="example"
+                                                style="width: 100%"
+                                            />
+                                        </a-modal>
+                                    </a-form-item>
+                                </a-col>
+                                <a-col
+                                    :span="24"
+                                    class="tw-flex tw-justify-center"
+                                >
+                                    <a-form-item class="tw-w-full">
+                                        <a-row justify="end">
+                                            <a-button>Cancel</a-button>
+
+                                            <a-button
+                                                type="primary"
+                                                class="tw-ml-2"
+                                                >Confirm</a-button
+                                            >
+                                        </a-row>
+                                    </a-form-item>
+                                </a-col>
+                            </a-row>
+                        </a-col>
+                    </a-row>
+                </a-form>
+            </a-card>
+        </a-col>
+    </a-row>
 </template>
 
 <script setup lang="ts">
---
+++ readme/ep 91 - 100/readme_ep95.md
@@ -0,0 +1,167 @@
+# Nuxt 3 CMS Stock Course EP.94 - Workshop - Frontend - Edit Product (Part 1)
+
+## Outcome
+
+-   [x] Create `edit.vue` in `~/pages/stock/edit.vue`
+
+## Documentation for this episode
+
+-   X
+
+## Setup
+
+1. Create `[id].vue` in `~/pages/stock/edit/[id].vue`
+
+```vue
+<!-- ~/pages/stock/edit/[id].vue -->
+
+<template>
+    <div>
+        {{ route.params.id }}
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: "default",
+});
+const route = useRoute();
+</script>
+
+<style scoped></style>
+```
+
+2. Add `router` when press edit button in `~/pages/stock/index.vue`
+
+```vue
+<!-- ~/pages/stock/index.vue -->
+
+<template>
+    <a-row :gutter="[0, 10]">
+        <a-col :span="24" class="tw-my-1 tw-mt-2">
+            <a-row :gutter="[0, 10]" align="middle" justify="center">
+                <a-col
+                    :span="6"
+                    v-for="(item, i) in stockCardList"
+                    :key="i"
+                    class="tw-pr-2"
+                >
+                    <StockCard
+                        :title="item.title"
+                        :amount="item.amount"
+                        :color="item.color"
+                        :icon="item.icon"
+                    ></StockCard>
+                </a-col>
+            </a-row>
+        </a-col>
+        <a-col :span="24" class="tw-my-1">
+            <a-col :span="24" class="tw-my-1">
+                <a-card
+                    class="tw-w-full tw-min-h-[75vh] tw-rounded-lg tw-drop-shadow-md"
+                >
+                    <a-row align="middle" justify="center" :gutter="[0, 10]">
+                        <a-col :span="24">
+                            <a-row justify="space-between" :gutter="[0, 10]">
+                                <a-col :span="22">
+                                    <a-auto-complete
+                                        size="large"
+                                        class="tw-w-full tw-drop-shadow-sm hover:tw-drop-shadow-md tw-transition-all"
+                                        :options="
+                                            productStore.autoCompleteOptions
+                                        "
+                                        @search="productStore.debouncedSearch"
+                                        @select="productStore.onSelect"
+                                        :default-active-first-option="false"
+                                        :filter-option="false"
+                                    >
+                                        <template #placeholder>
+                                            <SearchOutlined /> Input search
+                                            text</template
+                                        >
+                                    </a-auto-complete>
+                                </a-col>
+                                <a-col :span="2">
+                                    <a-button
+                                        @click="$router.push('/stock/create')"
+                                        class="tw-w-full tw-drop-shadow-sm hover:tw-drop-shadow-md tw-transition-all tw-flex tw-items-center tw-justify-center tw-border-white"
+                                        shape="round"
+                                        size="large"
+                                        type="primary"
+                                    >
+                                        <span v-if="breakpointState.sm">
+                                            NEW
+                                        </span>
+                                        <template #icon>
+                                            <PlusCircleFilled
+                                        /></template>
+                                    </a-button>
+                                </a-col>
+                            </a-row>
+                        </a-col>
+                        <a-col :span="24">
+                            <ProductTable
+                                :products="productStore.products"
+                                @handleClickDelete="handleClickDelete"
+                                @handleClickEdit="handleClickEdit"
+                            />
+                        </a-col>
+                    </a-row>
+                </a-card>
+            </a-col>
+        </a-col>
+    </a-row>
+</template>
+
+<script setup lang="ts">
+import { Grid } from "ant-design-vue";
+import {
+    ShoppingCartOutlined,
+    SearchOutlined,
+    RollbackOutlined,
+    GiftOutlined,
+    PlusCircleFilled,
+} from "@ant-design/icons-vue";
+
+definePageMeta({
+    layout: "default",
+});
+
+const router = useRouter();
+
+const stockCardList = ref([
+    {
+        title: "Total",
+        amount: 1800,
+        icon: ShoppingCartOutlined,
+        color: "#00B98D",
+    },
+    { title: "Sold out", amount: 20, icon: SearchOutlined, color: "#058C42" },
+    { title: "Return", amount: 4, icon: RollbackOutlined, color: "#04471C" },
+    {
+        title: "Discount",
+        amount: 2,
+        icon: GiftOutlined,
+        color: "#0D2818",
+    },
+]);
+
+const { useBreakpoint } = Grid;
+
+const breakpointState = useBreakpoint();
+
+const productStore = useProductStore();
+
+productStore.loadProducts();
+
+const handleClickDelete = (id: number) => {
+    // productStore.deleteProduct(id);
+};
+
+const handleClickEdit = (id: number) => {
+    router.push(`/stock/edit/${id}`);
+};
+</script>
+
+<style scoped></style>
+```
